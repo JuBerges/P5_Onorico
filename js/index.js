88fd77f16ac062ab69d46a2835c843317bd98be3
Wade Spectre <mbergesjulien@gmail.com>
2020-11-13 16:57:59 +0100

    Ajout fonction js prix

--- js/index.js
+++ js/index.js
@@ -2,12 +2,15 @@
 let tedImg = document.getElementById('tedImg');
 let tedName = document.getElementById('tedName');
 let tedDes = document.getElementById('tedDes');
+let tedPrice = document.getElementById('tedPrice');
 let camImg = document.getElementById('camImg');
 let camName = document.getElementById('camName');
 let camDes = document.getElementById('camDes');
+let camPrice = document.getElementById('camPrice');
 let furImg = document.getElementById('furImg');
 let furName = document.getElementById('furName');
 let furDes = document.getElementById('furDes');
+let furPrice = document.getElementById('furPrice');
 
 //functions for fetch request
 async function askTed() {
@@ -25,19 +28,27 @@ async function askCam() {
     let response = await promise.json()
     return response
   }
+//fonction pour convertir le prix
+  function convertCents(num) {
+    let result = num / 100
+    return Math.round(result).toFixed(2)+" €"
+  };  
 
   askTed().then(function(response) {
     tedImg.src = response[0].imageUrl
     tedName.innerHTML = response[0].name
-    tedDes.innerHTML = response[0].description 
+    tedDes.innerHTML = response[0].description
+    tedPrice.innerText = convertCents(response[0].price);
   });
   askCam().then(function(response) {
     camImg.src = response[0].imageUrl
     camName.innerHTML = response[0].name
     camDes.innerHTML = response[0].description
+    camPrice.innerText = convertCents(response[0].price) 
   });
   askFur().then(function(response) {
     furImg.src = response[0].imageUrl
     furName.innerHTML = response[0].name
     furDes.innerHTML = response[0].description  
+    furPrice.innerText = convertCents(response[0].price)
   });
