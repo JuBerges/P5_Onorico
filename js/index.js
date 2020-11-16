7200df9770d11cb6eb5e441c96863eef88d13d1a
WadeSpectre <mbergesjulien@gmail.com>
2020-11-16 15:09:06 +0100

    Ours changés pour Cameras

--- js/index.js
+++ js/index.js
@@ -1,54 +1,41 @@
-//teddies
-let tedImg = document.getElementById('tedImg');
-let tedName = document.getElementById('tedName');
-let tedDes = document.getElementById('tedDes');
-let tedPrice = document.getElementById('tedPrice');
-//Item page
-let itemImg = document.getElementById('itemImg');
-let itemName = document.getElementById('itemName');
-let itemId = document.getElementById('itemId');
-let itemDes = document.getElementById('itemDes');
-let itemPrice = document.getElementById('itemPrice');
-
 //functions for fetch request "à simplifier?"
-async function askTed() {
-  let promise = await fetch("http://localhost:3000/api/teddies")
-  let response = await promise.json()
-  return response
+async function askCam() {
+  let promise = await fetch("http://localhost:3000/api/cameras");
+  let response = await promise.json();
+  return response;
 }
 //fonction pour convertir le prix
-  function convertCents(num) {
-    let result = num / 100
-    return Math.round(result).toFixed(2)+" €"
-  };  
+function convertCents(num) {
+  let result = num / 100;
+  return Math.round(result).toFixed(2) + " €";
+}
 //functions for fetch request
-  askTed().then(function(response) {
-    tedImg1.src = response[0].imageUrl
-    tedName1.innerHTML = response[0].name
-    tedDes1.innerHTML = response[0].description
-    tedPrice1.innerText = convertCents(response[0].price)
+askCam().then(function (response) {
+  camImg1.src = response[0].imageUrl;
+  camName1.innerHTML = response[0].name;
+  camDes1.innerHTML = response[0].description;
+  camPrice1.innerText = convertCents(response[0].price);
 
-    tedImg2.src = response[1].imageUrl
-    tedName2.innerHTML = response[1].name
-    tedDes2.innerHTML = response[1].description
-    tedPrice2.innerText = convertCents(response[1].price)
+  camImg2.src = response[1].imageUrl;
+  camName2.innerHTML = response[1].name;
+  camDes2.innerHTML = response[1].description;
+  camPrice2.innerText = convertCents(response[1].price);
 
-    tedImg3.src = response[2].imageUrl
-    tedName3.innerHTML = response[2].name
-    tedDes3.innerHTML = response[2].description
-    tedPrice3.innerText = convertCents(response[2].price)
+  camImg3.src = response[2].imageUrl;
+  camName3.innerHTML = response[2].name;
+  camDes3.innerHTML = response[2].description;
+  camPrice3.innerText = convertCents(response[2].price);
 
-    tedImg4.src = response[3].imageUrl
-    tedName4.innerHTML = response[3].name
-    tedDes4.innerHTML = response[3].description
-    tedPrice4.innerText = convertCents(response[3].price)
+  camImg4.src = response[3].imageUrl;
+  camName4.innerHTML = response[3].name;
+  camDes4.innerHTML = response[3].description;
+  camPrice4.innerText = convertCents(response[3].price);
 
-    tedImg5.src = response[4].imageUrl
-    tedName5.innerHTML = response[4].name
-    tedDes5.innerHTML = response[4].description
-    tedPrice5.innerText = convertCents(response[4].price)
-  });
+  camImg5.src = response[4].imageUrl;
+  camName5.innerHTML = response[4].name;
+  camDes5.innerHTML = response[4].description;
+  camPrice5.innerText = convertCents(response[4].price);
+});
 
 //======================================================
 //For item page
-
